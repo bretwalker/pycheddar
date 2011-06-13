1de2eb4b01deff50ae587d9c95f5fbbd055bfac2
Bret Walker <bretwalker@gmail.com>
2011-06-12 22:41:53 -0400

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,33 @@
+import os
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+setup(
+    name = 'pycheddar',
+    version = '1.0',
+    description = 'Pythonic Python wrapper for CheddarGetter.'
+    license = 'BSD',
+    long_description = read('README.rst'),
+    url = 'https://github.com/bretwalker/pycheddar',
+    
+    
+    py_modules =  ['pycheddar'],
+    
+    classifiers = (
+        'Development Status :: 4 - Beta',
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 2.5',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python',
+    ),
+)
